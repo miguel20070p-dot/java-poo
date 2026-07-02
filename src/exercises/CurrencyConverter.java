e502bceb53a1f0a699823fb4429b989221f6ca82
Miguel <miguel20070p@gmail.com>
2026-07-02 16:16:46 -0300

    Exercicio 80 - CurrencyConverter com metodo estatico (Secao 8 concluida)

---
+++ src/exercises/CurrencyConverter.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class CurrencyConverter {
+    public  static double convert(double quotation, double  amount) {
+        return quotation * amount * 1.06;
+    }
+
+}
---
+++ src/exercises/ProgramCurrencyConverter.java
@@ -0,0 +1,19 @@
+package exercises;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramCurrencyConverter {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("What is the dollar price? ");
+        double price= sc.nextDouble();
+        System.out.print("How many dollars will be bought? ");
+        double bought = sc.nextDouble();
+        double result = CurrencyConverter.convert(price, bought);
+        System.out.printf("Amount to be paid in reais = %.2f%n",result);
+        sc.close();
+    }
+}
